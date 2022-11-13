ba02beef3e1ae969ebe3be91a0bc183863788f37
Hichamoubari <barisham30@gmail.com>
2022-11-13 18:47:29 +0100

    Spring cloud Open Feign Use Case

--- custmer-service/src/main/java/org/sid/custmerservice/CustmerServiceApplication.java
+++ custmer-service/src/main/java/org/sid/custmerservice/CustmerServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class CustmerServiceApplication {
@@ -15,7 +16,8 @@ public class CustmerServiceApplication {
     }
 
     @Bean
-    CommandLineRunner start(CustomerRepository customerRepository){
+    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration restConfiguration){
+        restConfiguration.exposeIdsFor(Customer.class);
         return args -> {
           customerRepository.save(new Customer(null,"Hicham","hicham@gmail.com"));
           customerRepository.save(new Customer(null,"Ghita","ghita@gmail.com"));
